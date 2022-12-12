40f118f1546da97d07bfc5d44cac0f9803748661
Evgeniy Kochu <evgeniykochu@gmail.com>
2022-12-12 17:43:41 +0300

    tasks

--- tasks.js
+++ tasks.js
@@ -6,3 +6,21 @@
 	}
 }
 
+{
+	let arr = [1, 2, 3, 4, 5, 6, 7];
+
+	function inBetween(a, b) {
+		return function f(num) {
+			return num >= a && num <= b;
+		}
+	}
+
+	function inArray(arr) {
+		return function f(num) {
+			return arr.includes(num);
+		}
+	}
+
+	console.log(arr.filter(inBetween(3, 6)))
+	console.log(arr.filter(inArray([1, 2, 10])))
+}
